39eebe2742de1117998490bbf76f40bd9e8a34ec
Andrea <andrea.giordano5@studio.unibo.it>
2020-11-20 12:46:02 +0100

    added custom css and env

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  publicPath: '/giordo94/'
+  pluginOptions: {
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: false
+    }
+  }
 }
